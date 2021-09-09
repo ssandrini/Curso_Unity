c3c39efd59e9f96d67261a70b524487389bad3d3
ssandrini <ssandrini@itba.edu.ar>
2021-09-08 22:02:25 -0300

    Adding triggers and end of the game

---
+++ Farm/Assets/Scripts/CheckCollisions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CheckCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+
+    }
+}
--- Farm/Assets/Scripts/DestroyOutOfBounds.cs
+++ Farm/Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,9 +10,17 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.transform.position.z > topBound || this.transform.position.z < botBound)
+        if (this.transform.position.z > topBound )
         {
             Destroy(this.gameObject);
+        }
+
+        if (this.transform.position.z < botBound)
+        {
+            Debug.Log("GAME OVER");
+            Destroy(this.gameObject);
+
+            Time.timeScale = 0;
         }
     }
 }
